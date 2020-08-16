09cc24ab7572c236345720595f7df610528daa42
Matt Wright <mttwright@gmail.com>
2020-08-16 22:51:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,7 +13,12 @@ class Application
       end
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        resp.write ""
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)

679425a269b9abf4e7b350a3b2d7ef004008f1ab
Matt Wright <mttwright@gmail.com>
2020-08-16 23:02:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,12 +22,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      if handle_search(search_term) == "#{search_term} is one of our items"
+        @@cart.push(search_term)
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
 
     resp.finish
   end
+  
+  def add_item_to_cart
 
   def handle_search(search_term)
     if @@items.include?(search_term)
